93beac3137ba7885ead689c3c38ea1154f83f2dd
darrenjcruz <81451114+darrenjcruz@users.noreply.github.com>
2023-09-22 21:55:25 -0700

    Adding more info

--- LexicalAnalyzer.cpp
+++ LexicalAnalyzer.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
 using namespace std;
 
+//Lexer Pseudocode
+
+/*
+function lexer()
+{
+    repeat
+        getchar();
+        if input char terminates a token
+            AND it is an accepting state then
+                isolate the token/lexeme
+                decrement the CP if necessary
+        else lookup FSM (current state, input char);
+    until (token found) or (no more input)
+
+    if token found then
+        return(token)
+}
+*/
+
+
 int main()
 {
-  cout << "Hello, world!\n";
+    cout << "Hello, world!\n";
 }
+
+
+//input_scode.txt
+
+/*
+while(t<upper)s=22.00;
+
+output:
+Token       Lexeme
+keyword     while
+separator   (
+identifier  t
+operator    <
+identifier  upper
+separator   )
+identifier  s
+operator    =
+real        22.00
+separator   ;
+*/
