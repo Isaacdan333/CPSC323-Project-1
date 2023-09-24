2010454f4bb9b28d07b9510c1f78f997372dcd21
lyquochao84 <95463211+lyquochao84@users.noreply.github.com>
2023-09-24 08:07:30 -0700

    Base code

--- LexicalAnalyzer.cpp
+++ LexicalAnalyzer.cpp
@@ -1,8 +1,20 @@
+/*
+    Project: CPSC 323 - Project 1
+    Group members: Hao Ly, ..., ...
+    Program: Lexical Analyzer
+    Input file: input_scode.txt
+    Output file: output.doc
+    Language: C++
+*/
+
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+
 using namespace std;
 
 //Lexer Pseudocode
-
 /*
 function lexer()
 {
@@ -20,10 +32,48 @@ function lexer()
 }
 */
 
+// Our code
+// Define different types of token
+enum TokenType {
+    Keyword,
+    Separator,
+    Identifier,
+    Operator,
+    Real,
+    Unknown
+};
 
-int main()
-{
-    cout << "Hello, world!\n";
+// Type of token & lexeme
+struct Lexical {
+    TokenType token;
+    string lexeme;
+};
+
+// Function takes a string code as input and returns a vector of Token objects
+vector<Lexical> tokenize(const string& code) {
+    vector<Lexical> tokens;
+    regex keyword_regex("while|if|else|for");
+    regex separator_regex("[(),;{}]");
+    regex identifier_regex("[a-zA-Z_][a-zA-Z0-9_]*");
+    regex realnumber_regex("[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?");
+    regex operator_regex("[+\\-*/=]");
+
+    // Works from here
+    // ....
+}
+
+// Main function 
+int main() {
+    ifstream inputFile;
+    inputFile.open("input_scode.txt");
+
+    if (!inputFile) {
+        cerr << "Failed to open the input file!" << endl;
+        return 1;
+    }
+
+    // Works from here
+    // ....
 }
 
 
