7fa75d36a18ae5120310a741024f92abaa47cd5f
lyquochao84 <95463211+lyquochao84@users.noreply.github.com>
2023-10-01 11:36:00 -0700

    Complete Program

--- LexicalAnalyzer.cpp
+++ LexicalAnalyzer.cpp
@@ -1,17 +1,17 @@
 /*
     Project: CPSC 323 - Project 1
-    Group members: Hao Ly, ..., ...
+    Group members: Hao Ly, Darren Cruz, Isaac Perez
     Program: Lexical Analyzer
     Input file: input_scode.txt
-    Output file: output.doc
+    Output file: output.txt
     Language: C++
 */
 
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <regex>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
@@ -31,111 +31,139 @@ struct Lexical {
     string lexeme;
 };
 
-// Function takes a string code as input and returns a vector of Token objects
-vector<Lexical> tokenize(const string& code) {
+// Function to check if a character is a whitespace
+bool isWhitespace(char c) {
+    return (c == ' ' || c == '\t' || c == '\n' || c == '\r');
+}
+
+// Function to check if a character is a terminating character (It means the end of our code or tokens)
+bool isTerminatingChar(char c) {
+    return (c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == ';' || c == '}');
+}
+
+// Lexical Analyzer
+vector<Lexical> lexer(const string& inputCode) {
     vector<Lexical> tokens;
-    regex keyword_regex("while|if|else|for");
-    regex separator_regex("[(),;{}]");
-    regex identifier_regex("[a-zA-Z_][a-zA-Z0-9_]*");
-    regex realnumber_regex("[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?");
-    regex operator_regex("[+\\-*/=<>,]");
 
-    // Initialize start and end positions
-    int start = 0;
-    int end = 0;
+    int currentPos = 0; // Keep track the index of each character in our code
+    int codeLength = inputCode.length();
 
-    while (end < code.length()) {
-        // Find the next space or end of the string
-        end = code.find(" ", start);
+    while (currentPos < codeLength) {
+        char currentChar = inputCode[currentPos];
 
-        // If no space is found, set end to the end of the string
-        if (end == string::npos) {
-            end = code.length();
+        // Skip whitespace characters
+        if (isWhitespace(currentChar)) {
+            currentPos++;
+            continue;
         }
 
-        // Extract the substring
-        string lexeme = code.substr(start, end - start);
-        Lexical lex;
+        string lexeme;
+        TokenType tokenType = Unknown;
 
-        // Check for token type
-        if (regex_match(lexeme, keyword_regex)) {
-            lex.token = Keyword;
-        } 
-        else if (regex_match(lexeme, separator_regex)) {
-            lex.token = Separator;
-        } 
-        else if (regex_match(lexeme, identifier_regex)) {
-            lex.token = Identifier;
-        } 
-        else if (regex_match(lexeme, realnumber_regex)) {
-            lex.token = Real;
-        } 
-        else if (regex_match(lexeme, operator_regex)) {
-            lex.token = Operator;
-        } 
-        else {
-            lex.token = Unknown;
+        // Check if the current character is a separator
+        if (currentChar == '(' || currentChar == ')' || currentChar == ';' || currentChar == '{' || currentChar == '}') {
+            tokenType = Separator;
+            lexeme = currentChar;
+            currentPos++;
         }
 
-        lex.lexeme = lexeme;
-        tokens.push_back(lex);
+        // Check if the current character is an operator
+        else if (currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/' || currentChar == '=' || currentChar == '<' || currentChar == '>') {
+            tokenType = Operator;
+            lexeme = currentChar;
+            currentPos++;
+        }
 
-        // Update start position for the next iteration
-        start = end + 1;
+        // Check if the current character is a digit (real number)
+        else if (isdigit(currentChar)) {
+            while (currentPos < codeLength && (isdigit(currentChar) || currentChar == '.')) {
+                lexeme += currentChar;
+                currentChar = inputCode[++currentPos];
+            }
+            tokenType = Real;
+        }
+
+        // Check if the current character is a letter (identifier or keyword)
+        else if (isalpha(currentChar) || currentChar == '_') {
+            while (currentPos < codeLength && (isalnum(currentChar) || currentChar == '_')) {
+                lexeme += currentChar;
+                currentChar = inputCode[++currentPos];
+            }
+
+            // Check if the lexeme is a keyword
+            if (lexeme == "while" || lexeme == "if" || lexeme == "else" || lexeme == "for") {
+                tokenType = Keyword;
+            } 
+            else {
+                tokenType = Identifier;
+            }
+        }
+
+        // Add the token to the list
+        tokens.push_back({ 
+            tokenType, 
+            lexeme 
+        });
     }
 
     return tokens;
 }
 
 int main() {
-    ifstream inputFile;
-    inputFile.open("input_scode.txt");
-
+    ifstream inputFile("input_scode.txt");
     if (!inputFile) {
         cerr << "Failed to open the input file!" << endl;
         return 1;
     }
 
     // Read the entire input file into a string
-    string input_str((istreambuf_iterator<char>(inputFile)), istreambuf_iterator<char>());
-
-    // Open the output file for writing
-    ofstream outfile("output.txt");
-    outfile << "+-------------------+-------------------+" << endl;
-    outfile << "|       Token       |       Lexeme      |" << endl;
-    outfile << "+-------------------+-------------------+" << endl;
+    string inputCode((istreambuf_iterator<char>(inputFile)), istreambuf_iterator<char>());
+    inputFile.close();
 
     // Tokenize the input string
-    vector<Lexical> tokens = tokenize(input_str);
+    vector<Lexical> tokens = lexer(inputCode);
 
-    // Process and write tokens to the output file
-    for (const Lexical& token : tokens) {
-        string tokenTypeStr;
-
-        switch (token.token) {
-            case Keyword:
-                tokenTypeStr = "Keyword";
-                break;
-            case Separator:
-                tokenTypeStr = "Separator";
-                break;
-            case Identifier:
-                tokenTypeStr = "Identifier";
-                break;
-            case Real:
-                tokenTypeStr = "Real";
-                break;
-            case Operator:
-                tokenTypeStr = "Operator";
-                break;
-            case Unknown:
-                tokenTypeStr = "Unknown";
-                break;
-        }
-        outfile << "|           " << tokenTypeStr << "      |       " << token.lexeme << "      |" << endl;
+    // Open the output file for writing
+    ofstream outputFile("output.txt");
+    if (!outputFile) {
+        cerr << "Failed to open the output file!" << endl;
+        return 1;
     }
 
-    inputFile.close();
-    outfile.close();
+    // Write the header
+    outputFile << "+-------------------+-------------------+" << endl;
+    outputFile << "|       Token       |       Lexeme      |" << endl;
+    outputFile << "+-------------------+-------------------+" << endl;
+
+    // Write the tokens and lexemes
+    for (const Lexical& token : tokens) {
+        outputFile << "| " << setw(18) << left;
+        switch (token.token) {
+            case Keyword:
+                outputFile << "Keyword";
+                break;
+            case Separator:
+                outputFile << "Separator";
+                break;
+            case Identifier:
+                outputFile << "Identifier";
+                break;
+            case Operator:
+                outputFile << "Operator";
+                break;
+            case Real:
+                outputFile << "Real";
+                break;
+            case Unknown:
+                outputFile << "Unknown";
+                break;
+        }
+        outputFile << "| " << setw(17) << left << token.lexeme << " | " << endl;
+    }
+
+    // Close the output file
+    outputFile.close();
+
+    cout << "Lexical analysis completed. Results written to 'output.txt'." << endl;
     return 0;
 }
